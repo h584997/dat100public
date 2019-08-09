9f591a6242176de792a4ff9f210dc5db5ecfc49d
Lars Kristensen <lmkr@hib.no>
2019-08-09 16:01:53 +0200

    Noen eksempler til F03

---
+++ forelesninger/F03SetningerUttrykk/src/no/hvl/dat100/Uttrykk.java
@@ -0,0 +1,34 @@
+package no.hvl.dat100;
+
+public class Uttrykk {
+
+	public static void main(String[] args) {
+
+		double pris;
+		
+		int rabatt;
+		
+		int antallStudenter;
+		
+		int i,j;
+				
+		int x = 2 + 2;
+
+		int y = x + 3;
+
+		y = y * 2;
+
+		double snitt;
+
+		snitt = 5.0 + 4.0 / 2;
+
+		int alder1, alder2; 
+		
+		alder1 = 5;
+		alder2 = 4;
+
+		snitt = (alder1 + alder2) / 2;
+		
+		snitt = (alder1 + alder2) / 2.0;
+	}
+}
