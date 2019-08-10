9b868175373aae956db3451acd0b894a3678ea4f
Lars Michael Kristensen <lmkr@hvl.no>
2019-08-10 22:55:45 +0200

    F03 eksempler oppdatert

--- forelesninger/F03SetningerUttrykk/src/no/hvl/dat100/Uttrykk.java
+++ forelesninger/F03SetningerUttrykk/src/no/hvl/dat100/Uttrykk.java
@@ -2,6 +2,8 @@ package no.hvl.dat100;
 
 public class Uttrykk {
 
+	final double MVA = 0.25;
+	
 	public static void main(String[] args) {
 
 		double pris;
@@ -22,6 +24,8 @@ public class Uttrykk {
 
 		snitt = 5.0 + 4.0 / 2;
 
+		snitt = (5.0 + 4.0) / 2;
+		
 		int alder1, alder2; 
 		
 		alder1 = 5;
@@ -30,5 +34,26 @@ public class Uttrykk {
 		snitt = (alder1 + alder2) / 2;
 		
 		snitt = (alder1 + alder2) / 2.0;
+		
+		/*
+		double desimal;
+		int heltall = 5;
+				
+		desimal = heltall;
+		*/
+		
+		/*
+		double desimal = 2.8;
+		int heltall;
+				
+		heltall = (int)desimal;
+		*/
+		
+		double desimal = 2.8;
+		int heltall;
+				
+		heltall = (int)(desimal + 0.5);
+		
+		
 	}
 }
